1ef12cbe591cc9b26501ebf3dcb81bb5529e9363
Rayan Outili <rayan.outili@etu.unice.fr>
2024-03-15 13:07:54 +0100

    rm tests

--- src/test/java/iut/outili/profTrackerAPI/ProfTrackerApiApplicationTests.java
+++ src/test/java/iut/outili/profTrackerAPI/ProfTrackerApiApplicationTests.java
@@ -1,13 +1,8 @@
 package iut.outili.profTrackerAPI;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProfTrackerApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
